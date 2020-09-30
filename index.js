974538bfb8448019c34e8e63670875e51b9aefcd
geethaVignesh <72148988+geethaVignesh@users.noreply.github.com>
2020-09-30 20:33:24 +0530

    image upload code

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const multer = require('multer')
+
+const uploadImage = require('./helpers/helpers')
+
+const app = express()
+
+const multerMid = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    // no larger than 5mb.
+    fileSize: 5 * 1024 * 1024,
+  },
+});
+
+app.disable('x-powered-by')
+app.use(multerMid.single('file'))
+app.use(bodyParser.json({limit:'50mb'}))
+app.use(bodyParser.urlencoded({extended: false}))
+app.get('/', function (req, res) {
+  res.send('Welcome to Cloud Deployment')
+})
+
+app.post('/uploads', async (req, res, next) => {
+  console.log("request",req.file)
+  try {
+    const myFile = req.file
+    const imageUrl = await uploadImage(myFile)
+
+    res
+      .status(200)
+      .json({
+        message: "Upload was successful",
+        data: imageUrl
+      })
+  } catch (error) {
+    next(error)
+  }
+})
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    error: err,
+    message: 'Internal server error!',
+  })
+  next()
+})
+
+app.listen(9001, () => {
+  console.log('app now listening for requests!!!')
+})
